d49a8b8d9924a1481960202f1a94e6b445d949f8
Asif <saluasif@gmail.com>
2015-10-25 16:16:13 +0300

    updated setup

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 import os
 from setuptools import setup
  
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
- 
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
  
 setup(
     name = 'django-crudbuilder',
-    version = '0.1',
+    version = '0.0.3',
     packages = ['crudbuilder'],
     include_package_data = True,
     license = 'BSD License',
     description = 'A simple Django CRUD builder',
-    long_description = README,
     url = 'https://github.com/asifpy/django-crudbuilder/archive/master.tar.gz',
     author = 'Asif Jamadar',
     author_email = 'saluasif@gmail.com',
