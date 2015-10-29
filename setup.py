42127fa64b6e0094efc5855023ef1f329b9162eb
Asif <saluasif@gmail.com>
2015-10-29 17:51:11 +0300

    update release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
  
 setup(
     name = 'django-crudbuilder',
-    version = '0.0.4',
+    version = '0.0.5',
     packages = ['crudbuilder'],
     include_package_data = True,
     license = 'BSD License',
